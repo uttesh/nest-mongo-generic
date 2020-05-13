2e9428b6ba5f2e567d9ab00eeb598417629441c6
uttesh <uttesh@gmail.com>
2020-05-13 17:11:02 +0530

    initial

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/giide'),
+   UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.service.ts
@@ -0,0 +1,34 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
+
+  async create(user: User): Promise<User> {
+    const createdUser = new this.userModel(user);
+    return createdUser.save();
+  }
+
+  async update(id, user: User): Promise<User> {
+    const updatedCustomer = await this.userModel
+        .findByIdAndUpdate(id, user, { new: true });
+    return updatedCustomer;
+  }
+
+  async delete(id): Promise<any> {
+    const deletedCustomer = await this.userModel.findByIdAndRemove(id);
+    return deletedCustomer;
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async get(id): Promise<User> {
+      const customer = await this.userModel.findById(id).exec();
+      return customer;
+  }
+}
