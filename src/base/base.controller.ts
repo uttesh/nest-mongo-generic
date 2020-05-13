1d19285606fd0d4280a196078bbf6417bdba3269
uttesh <uttesh@rivetsys.com>
2020-05-14 00:04:41 +0530

    added the base.controller.ts for the crud related operations

---
+++ src/base/base.controller.ts
@@ -0,0 +1,57 @@
+import { Controller, Get, Res, HttpStatus, Post, Body, Put, Query, NotFoundException, Delete, Param } from '@nestjs/common';
+import { IBaseService } from './IBase.service'
+import { BaseEntity } from './base.entity';
+
+export class BaseController<T extends BaseEntity> {
+
+  constructor(private readonly baseService: IBaseService<T>) {}
+  
+  @Get()
+  async getAll(@Res() res) {
+      const entities = await this.baseService.findAll();
+      return res.status(HttpStatus.OK).json(entities);
+  }
+
+  @Get(':id')
+  async getCustomer(@Res() res, @Param('id') id) {
+      const entity = await this.baseService.get(id);
+      if (!entity){
+        throw new NotFoundException('Entity does not exist!');
+      }
+      return res.status(HttpStatus.OK).json(entity);
+
+  }
+
+  @Post()
+  async add(@Res() res, @Body() t: T) {
+      const updatedEntity = await this.baseService.create(t);
+      return res.status(HttpStatus.OK).json({
+          message: 'Entity has been created successfully',
+          updatedEntity
+      });
+  }
+
+  @Put(':id')
+  async update(@Res() res, @Query('id') id, @Body() t: T) {
+      const entity = await this.baseService.update(t);
+      if (!entity) {
+        throw new NotFoundException('Entity does not exist!');
+      } 
+      return res.status(HttpStatus.OK).json({
+          message: 'Entity has been successfully updated',
+          entity
+      });
+  }
+
+  @Delete(':id')
+  async deleteCustomer(@Res() res, @Query('id') id) {
+      const entity = await this.baseService.delete(id);
+      if (!entity) {
+        throw new NotFoundException('Entity does not exist');
+      }
+      return res.status(HttpStatus.OK).json({
+          message: 'Entity has been deleted',
+          entity
+      })
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,58 +1,11 @@
 import { Controller, Get, Res, HttpStatus, Post, Body, Put, Query, NotFoundException, Delete, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.model';
+import { BaseController } from 'src/base/base.controller';
 
 @Controller('user')
-export class UserController {
-    constructor(private userService: UserService) { }
-
-    @Get()
-    async getAll(@Res() res) {
-        const users = await this.userService.findAll();
-        return res.status(HttpStatus.OK).json(users);
-    }
-
-    @Get(':id')
-    async getCustomer(@Res() res, @Param('id') userid) {
-        const user = await this.userService.get(userid);
-        if (!user){
-          throw new NotFoundException('User does not exist!');
-        }
-        return res.status(HttpStatus.OK).json(user);
-
-    }
-
-    @Post()
-    async add(@Res() res, @Body() user: User) {
-        const updatedUser = await this.userService.create(user);
-        return res.status(HttpStatus.OK).json({
-            message: 'User has been created successfully',
-            updatedUser
-        });
-    }
-
-    @Put(':id')
-    async update(@Res() res, @Query('id') id, @Body() user: User) {
-        const customer = await this.userService.update(user);
-        if (!customer) {
-          throw new NotFoundException('User does not exist!');
-        } 
-        return res.status(HttpStatus.OK).json({
-            message: 'Customer has been successfully updated',
-            customer
-        });
-    }
-
-    // Delete a customer
-    @Delete(':id')
-    async deleteCustomer(@Res() res, @Query('id') id) {
-        const customer = await this.userService.delete(id);
-        if (!customer) {
-          throw new NotFoundException('User does not exist');
-        }
-        return res.status(HttpStatus.OK).json({
-            message: 'Customer has been deleted',
-            customer
-        })
-    }
+export class UserController extends BaseController<User> {
+    constructor(private userService: UserService) {
+        super(userService);
+     }
 }
